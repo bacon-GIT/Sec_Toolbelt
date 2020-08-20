b6c4dcdc9b804908911f278febfb0c38c18dd9dc
Bacon-HAL <56642323+bacon-GIT@users.noreply.github.com>
2020-08-20 11:19:43 -0400

    Added color

--- subScanner.py
+++ subScanner.py
@@ -7,41 +7,36 @@ from requests.packages import urllib3
 
 # Program Settings
 urllib3.disable_warnings()
+# Color Settings
+TGREEN = '\033[32m' #   Green Text
+TRED = '\033[35m' #     Red Text
+ENDC = '\033[m' #       Reset Shell color
 
 
-def scan_domain(domain, iters):
+def scan_domain(domain, i, line):
 
     flag = True
-    i = 0
-    successful_conn = []
-    with open("domains.txt", "r", encoding="UTF-8") as domains:
-        for line in domains:
-            if i >= iters:
-                break
-            try:
-                i += 1
-                URL = "https://{}{}".format(domain, line)
-                page = requests.get(URL, verify=False, allow_redirects=flag)
-                print("Scan {} of {} returned a code of:".format(i, line), page)
+    try:
+        URL = "https://{}{}".format(domain, line)
+        page = requests.get(URL, verify=False, allow_redirects=flag)
+        print("Scan {} of {} returned a code of:".format(i, line), page)
 
-                print("Page data:\t", page)
-                page_response = page.status_code
+        print("Page data:\t", page)
+        page_response = page.status_code
 
-                if page_response == 200:
-                    logging.info("Successful Connection!!")
-                    successful_conn.append(line)
+        print(page.status_code)
 
-            except requests.exceptions.InvalidURL:
-                logging.warning(f"{line} Not Found :-(")
+        if page_response == 200:
+            print(TGREEN + "Successful Connection!!", ENDC)
+            return line
+        else:
+            print(TRED + "Page not found.", ENDC)
 
-            except requests.exceptions.TooManyRedirects:
-                flag = False
+    except requests.exceptions.InvalidURL:
+        logging.warning(TRED + f"{line} Not Found :-(", ENDC)
 
-    print(f"Successful Connections to: {URL}\n")
-    print("The following returned successful connection attempts:")
-    for x in successful_conn:
-        print("~~~~~~~~~~~")
-        print(x)
+    except requests.exceptions.TooManyRedirects:
+        flag = False
 
 
 def main():
@@ -53,7 +48,7 @@ def main():
     except IndexError:
         print("Debugging off, launch SubScanner with -d to turn it on.")
 
-    banner = '''
+    banner = TGREEN + '''
  _______           ______   _______  _______  _______  _        _        _______  _______ 
 (  ____ \|\     /|(  ___ \ (  ____ \(  ____ \(  ___  )( (    /|( (    /|(  ____ \(  ____ )
 | (    \/| )   ( || (   ) )| (    \/| (    \/| (   ) ||  \  ( ||  \  ( || (    \/| (    )|
@@ -62,16 +57,33 @@ def main():
       ) || |   | || (  \ \       ) || |      | (   ) || | \   || | \   || (      | (\ (   
 /\____) || (___) || )___) )/\____) || (____/\| )   ( || )  \  || )  \  || (____/\| ) \ \__
 \_______)(_______)|/ \___/ \_______)(_______/|/     \||/    )_)|/    )_)(_______/|/   \__/
-                                                                                          '''
+                                                                                          ''' + ENDC
     print(banner)
 
     to_scan = input("Please Enter A Domain:\t\t")
-    try:
-        iters = int(input("Number of domains to try:\t"))
-    except TypeError:
-        logging.error("Please enter the number of domains to scan.")
-    scan_domain(to_scan, iters)
+    while True:
+        try:
+            iters = int(input("Number of domains to try:\t"))
+            break
+        except ValueError:
+            logging.error("Please enter the number of domains to scan.")
+    flag = True
+    i = 1
+    successful_conn = []
 
+    threads = list()
+
+    with open("domains.txt", "r", encoding="UTF-8") as domains:
+        for line in domains:
+            if i > iters:
+                break
+            if scan_domain(to_scan, i, line):
+                successful_conn.append(line)
+            i += 1
+
+    for x in successful_conn:
+        print("~~~~~~~~~~~")
+        print(TGREEN + x)
 
 if __name__ == "__main__":
     main()
