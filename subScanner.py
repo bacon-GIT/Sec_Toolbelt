28ea18a947cdaaed711d516ad89d6e42875f231e
Bacon-HAL <56642323+bacon-GIT@users.noreply.github.com>
2020-09-01 00:49:44 -0400

    Added notes, cleaned it up quite a bit

--- subScanner.py
+++ subScanner.py
@@ -1,5 +1,4 @@
 import logging
-import threading
 import time
 import requests
 import sys
@@ -7,6 +6,7 @@ from requests.packages import urllib3
 
 # Program Settings
 urllib3.disable_warnings()
+
 # Color Settings
 TGREEN = '\033[32m' #   Green Text
 TRED = '\033[35m' #     Red Text
@@ -19,12 +19,8 @@ def scan_domain(domain, i, line):
     try:
         URL = "https://{}{}".format(domain, line)
         page = requests.get(URL, verify=False, allow_redirects=flag)
-        print("Scan {} of {} returned a code of:".format(i, line), page)
-
-        print("Page data:\t", page)
+        print("Scan {} of {} returned a code of:".format(i, line.rstrip()), page)
         page_response = page.status_code
-
-        print(page.status_code)
 
         if page_response == 200:
             print(TGREEN + "Successful Connection!!", ENDC)
@@ -32,15 +28,23 @@ def scan_domain(domain, i, line):
         else:
             print(TRED + "Page not found.", ENDC)
 
+    # Exceptions for invalid url's and too many redirects.
+    # The flag for too many redirects is set to True by default,
+    # as this is meant to catch edge cases where redirects return as
+    # successful connections. Can't remember why right now, should probably write
+    # documentation while actually writing the code. Don't pretend you're any better.
+
     except requests.exceptions.InvalidURL:
         logging.warning(TRED + f"{line} Not Found :-(", ENDC)
-
     except requests.exceptions.TooManyRedirects:
         flag = False
 
 
 def main():
 
+    # TODO:
+    #   -Replace this method with argparse because this is just messy
+    #   -Add verbosity flag to parse with argparse
     try:
         if ('d' or 'Debug') in sys.argv[1]:
             print("Debugging on!")
@@ -67,12 +71,14 @@ def main():
             break
         except ValueError:
             logging.error("Please enter the number of domains to scan.")
-    flag = True
+
+    # Lists and integer declaration, timer for testing against threaded version.
+    # (As of currently, threading does not make this program quicker, grequests might work though, haven't tried it yet)
     i = 1
     successful_conn = []
-
-    threads = list()
-
+    start_time = time.perf_counter()
+    
+    # Main brain of program
     with open("assets/domains.txt", "r", encoding="UTF-8") as domains:
         for line in domains:
             if i > iters:
@@ -81,9 +87,13 @@ def main():
                 successful_conn.append(line)
             i += 1
 
+    stop_time = time.perf_counter() - start_time
+    print(f"~~~~~~~~~~\nCompleted in:\t{stop_time}\n~~~~~~~~~~~")
+
     for x in successful_conn:
         print("~~~~~~~~~~~")
         print(TGREEN + x)
+
 
 if __name__ == "__main__":
     main()
